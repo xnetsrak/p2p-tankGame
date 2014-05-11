5b7742abaf252a884fd567e213e8897017850bbe
xnetsrak <xnetsrak@gmail.com>
2014-05-11 20:13:08 +0200

    KARSTEN ER FANTASTISK

--- src/org/tank/Model/Coordinator.java
+++ src/org/tank/Model/Coordinator.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.tank.Members.Bomb;
 import org.tank.Members.Shot;
 import org.tank.Members.Tank;
 import org.tank.Msg.CoordinatorUpdateMsg;
